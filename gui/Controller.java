04e3163e2d641e36265966d2d0d118e540ff53df
TonyChouteau <tony.chouteau@outlook.fr>
2020-01-09 16:50:30 +0100

    Init

---
+++ gui/Controller.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
+
+public class Controller {
+    
+}
